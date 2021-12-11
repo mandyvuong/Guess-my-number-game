764601d6a8290e7e81a8e8bc9f5ef3f29c7f9aa6
amanda vuong <mandyvuong@gmail.com>
2021-12-11 19:48:19 +0000

    manipulate css

--- script.js
+++ script.js
@@ -11,10 +11,17 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess);
   console.log(typeof guess);
 
+  // when no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -24,6 +31,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
