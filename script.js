cb7818a6f74c6f3f3d1454094cf0647fe32b3d79
amanda vuong <mandyvuong@gmail.com>
2021-12-11 19:22:42 +0000

    handling click event

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  console.log(typeof guess);
 
-document.querySelector('.message').textContent = 'Correct number!';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 15;
-document.querySelector('.score').textContent = 20;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
