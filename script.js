563df7bd9c56f1c07a0afa5c8133daca9e7f2ab8
amanda vuong <mandyvuong@gmail.com>
2021-12-11 19:13:41 +0000

    select and manipulate elements

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct number!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
